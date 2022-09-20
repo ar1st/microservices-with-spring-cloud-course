15f74f521e1a26b05eeac9ce72994567f403cb9c
TSACHLARIS Aristeidis <aristeidis.tsachlaris@sword-group.com>
2022-09-20 20:18:49 +0300

    Add naming server

---
+++ naming-server/src/main/java/com/aris/namingserver/NamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.aris.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
+
+}
