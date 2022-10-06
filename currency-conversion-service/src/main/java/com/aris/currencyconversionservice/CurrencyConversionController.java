0ef87dbd28a8e73b6d31d6e3e83280dba7e7994d
TSACHLARIS Aristeidis <aristeidis.tsachlaris@sword-group.com>
2022-10-06 20:29:42 +0300

    Add api gateway service

--- currency-conversion-service/src/main/java/com/aris/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/aris/currencyconversionservice/CurrencyConversionController.java
@@ -1,7 +1,6 @@
 package com.aris.currencyconversionservice;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
