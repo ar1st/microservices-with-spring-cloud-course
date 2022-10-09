29b66f55dc623c944465bf7c0682781354ac3742
TSACHLARIS Aristeidis <aristeidis.tsachlaris@sword-group.com>
2022-10-09 11:07:15 +0300

    Trace requests with zipkin

--- currency-exchange-service/src/main/java/com/aris/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/aris/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,6 @@
 package com.aris.currencyexchangeservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class CurrencyExchangeController {
 
     public static final String PORT = "local.server.port";
@@ -20,6 +22,7 @@ public class CurrencyExchangeController {
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 
+        log.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
 
         if (currencyExchange == null) {
